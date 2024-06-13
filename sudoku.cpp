d88dcab72a8b00ec53ecde172c4566da936e310f
Jacob Gipp <60865374+kerokerosuruna@users.noreply.github.com>
2024-06-13 17:58:13 -0400

    Add files via upload

--- sudoku.cpp
+++ sudoku.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <pthread.h>
 #include <unordered_set>
+#include <string.h>
 
 int board [9][9];
 
@@ -18,7 +19,9 @@ pthread_mutex_t square_mutex;
 //Checks in a row is valid
 void * check_row(void * arg){
     int row = *(static_cast<int*>(arg));
-    std::unordered_set<int> seen; //hashset = only unique characters
+    //hashset = only unique characters
+    //if something is already in the hashset, then we have already seen it in this row
+    std::unordered_set<int> seen;
     for (int i = 0; i < 9; i++){
         int val = board[row][i];
         if ((val == 0) || (seen.find(val) != seen.end())){
@@ -53,6 +56,7 @@ void * check_col(void * arg){
 void * check_square(void * arg){
     int square = *(static_cast<int*>(arg));
     std::unordered_set<int> seen;
+    //just a way for me to find which square i want this thread to look at
     int xorg = square%3;
     int yorg = int(square/3);
     for (int y = 0; y < 2; y++){
@@ -60,7 +64,7 @@ void * check_square(void * arg){
             int val = board[yorg+y][xorg+x];
             if ((val == 0) || (seen.find(val) != seen.end())){
                 pthread_mutex_lock(&square_mutex);
-                square_bits |= (1 << square); //set the bit so we could recover the exact column if we want
+                square_bits |= (1 << square); //set the bit so we could recover the exact square if we want
                 pthread_mutex_unlock(&square_mutex);
                 break;
             }
@@ -70,7 +74,13 @@ void * check_square(void * arg){
     return NULL;
 }
 
-int main(){
+int main(int argc, char **argv){
+    bool silent = true;
+    if (argc != 1){
+        if (strncmp(argv[1], "-v", 3) == 0){
+            silent = false;
+        }
+    }
     std::ifstream input("board.txt");
     int * p = &board[0][0];
     int number_of_rows = 0;
@@ -82,15 +92,20 @@ int main(){
             break;
         }
         if (number.length() != 9){
-            std::cout << "Invalid board dimensions\n";
+            if (silent == false){
+                std::cout << "Invalid board dimensions\n";
+            }
             return 1;
         }
         std::string::const_iterator it = number.begin();
         while (it != number.end()){
             if (!std::isdigit(*it)){
-                std::cout << "Board contains non-digit characters\n";
+                if (silent == false){
+                    std::cout << "Board contains non-digit characters\n";
+                }
                 return 1;
             }
+            //0 is asacii code 48, so since we get ascii codes we need to subtract 48 to get integers
             *p = static_cast<int>(*it) - 48;
             p++;
             it++;
@@ -98,23 +113,28 @@ int main(){
         number_of_rows++;
     }
     if (number_of_rows != 9){
-        std::cout << "Invalid board dimensions\n";
+        if (silent == false){
+            std::cout << "Invalid board dimensions\n";
+        }
         return 1;
     }
-    std::cout << "The board: \n";
-    for (int y = 0; y < 9; y++){
-        for (int x = 0; x < 9; x++){
-            std::cout << board[y][x];
-            if (x%3 == 2 && x != 8){
-                std::cout << "|";
+
+    if (silent == false) {
+        std::cout << "The board: \n";
+        for (int y = 0; y < 9; y++){
+            for (int x = 0; x < 9; x++){
+                std::cout << board[y][x];
+                if (x%3 == 2 && x != 8){
+                    std::cout << "|";
+                }
+            }
+            std::cout << std::endl;
+            if (y%3 == 2 && y != 8){
+                std::cout << "-----------\n";
             }
         }
         std::cout << std::endl;
-        if (y%3 == 2 && y != 8){
-            std::cout << "-----------\n";
-        }
     }
-    std::cout << std::endl;
     
     //threads and locks
     pthread_t row_threads[9];
@@ -135,15 +155,21 @@ int main(){
         col_args[i] = i;
         square_args[i] = i;
         if (pthread_create(&row_threads[i], NULL, check_row, &row_args[i]) != 0){
-            std::cout << "Something went wrong checking the rows\n";
+            if (silent == false){
+                std::cout << "Something went wrong checking the rows\n";
+            }
             return 1;
         }
         if (pthread_create(&col_threads[i], NULL, check_col, &col_args[i]) != 0){
-            std::cout << "Something went wrong checking the columns\n";
+            if (silent == false){
+                std::cout << "Something went wrong checking the columns\n";
+            }
             return 1;
         }
         if (pthread_create(&square_threads[i], NULL, check_square, &square_args[i]) != 0){
-            std::cout << "Something went wrong checking the squares\n";
+            if (silent == false){
+                std::cout << "Something went wrong checking the squares\n";
+            }
             return 1;
         }
     }
@@ -165,14 +191,20 @@ int main(){
     pthread_mutex_destroy(&col_mutex);
     pthread_mutex_destroy(&square_mutex);
 
+    //each of the bitsets are initalized to 0, so if there are any changes result will not be 0.
+    //it will be 0 otherwise
     int result = row_bits | col_bits | square_bits;
 
     if (result != 0){
-        std::cout << "This is not a valid sudoku board\n";
+        if (silent == false){
+            std::cout << "This is not a valid sudoku board\n";
+        }
         return 1;
     }
     else{
-        std::cout << "This is a valid sudoku board\n";
+        if (silent == false){
+            std::cout << "This is a valid sudoku board\n";
+        }
         return 0;
     }
 }
